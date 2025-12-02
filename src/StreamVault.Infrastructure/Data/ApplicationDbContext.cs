ae9f6df3aa4344cc092a3c3c1315d57163a73c6b
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-02 01:18:13 +0100

    feat(infra): implement database context, configurations and sql commands

---
+++ src/StreamVault.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StreamVault.Domain.Entities;
+using System.Reflection;
+
+namespace StreamVault.Infrastructure.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+    : IdentityDbContext<User>(options)
+{
+    public DbSet<Broadcast> Broadcasts { get; init; }
+    public DbSet<Category> Categories { get; init; }
+    public DbSet<BroadcastCategory> BroadcastCategories { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ src/StreamVault.Infrastructure/Sql/SqlCommands.cs
@@ -0,0 +1,30 @@
+﻿namespace StreamVault.Infrastructure.Sql;
+
+public static class SqlCommands
+{
+    public const string GetUserCount = @"SELECT COUNT(""Id"") FROM ""AspNetUsers"";";
+
+    public const string GetMaxViewCount = @"SELECT MAX(""ViewCount"") FROM ""Broadcasts"";";
+
+    public const string GetVarianceViewCount = @"SELECT VARIANCE(""ViewCount"") FROM ""Broadcasts"";";
+
+    public const string GetViewCountQuartiles = @"
+        WITH percentiles AS (
+            SELECT
+                percentile_cont(0.25) WITHIN GROUP (ORDER BY ""ViewCount"") AS Q1,
+                percentile_cont(0.50) WITHIN GROUP (ORDER BY ""ViewCount"") AS Q2,
+                percentile_cont(0.75) WITHIN GROUP (ORDER BY ""ViewCount"") AS Q3
+            FROM ""Broadcasts""
+        )
+        SELECT Q1, Q2, Q3 FROM percentiles;";
+
+    public const string GetMaxViewsPerCategory = @"
+        SELECT 
+            c.""Name"" AS CategoryName,
+            MAX(b.""ViewCount"") AS MaxViews
+        FROM ""Broadcasts"" b
+        JOIN ""BroadcastCategories"" bc ON b.""Id"" = bc.""BroadcastId""
+        JOIN ""Categories"" c ON bc.""CategoryId"" = c.""Id""
+        GROUP BY c.""Name""
+        ORDER BY MaxViews DESC;";
+}
